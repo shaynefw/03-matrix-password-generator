0bd7b3c2da0b1bdb6eb1bf3cdf6d816cec3a189b
swmac <shaynefw@gmail.com>
2023-01-19 17:59:53 -0500

    major changes to fix error, updated readme

--- README.md
+++ README.md
@@ -2,21 +2,15 @@
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+This password generator is a simple tool that allows users to create a secure, random password.
 
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
+## Table of Contents
 
 - [Installation](#installation)
 - [Usage](#usage)
 - [Credits](#credits)
 - [License](#license)
+- [Features](#features)
 
 ## Installation
 
@@ -26,21 +20,16 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
+To use the password generator, the user simply needs to select the desired password length and character types, and then click the "Generate Password" button. A new, secure password will be generated and displayed for the user to use.
 
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+---
 
-    ```md
-    ![alt text](assets/images/screenshot.png)
-    ```
+![webpage screenshot](./assets/images/Password%20Generator.png)
 
 ## Credits
 
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
+tutor Sean Brown-Byfield, classmates, instructors, Prof, &
+[MDN](https://developer.mozilla.org/en-US/)
 
 ## License
 
@@ -68,22 +57,23 @@ SOFTWARE.
 
 ---
 
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
 ## Badges
 
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+![JS](https://img.shields.io/badge/Javascript-49.6%25-orange)
+![HTML](https://img.shields.io/badge/HTML-16%25-blue)
+![CSS](https://img.shields.io/badge/CSS-34.4-green)
 
 ## Features
 
-If your project has a lot of features, list them here.
+The user can choose the length of the password (between 8 and 128 characters)
+The user can choose the types of characters to include in the password: lowercase letters, uppercase letters, numbers, and/or special characters
+The password is generated using a cryptographically secure random number generator
+The generated password is displayed to the user and can be easily copied to the clipboard
 
 ## How to Contribute
 
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+N/A
 
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 
+var upperCase = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var specialChar = ["!","@","#","$","%","^","&","*","(",")","-","+","=","{","}","|","/",":",";",",",".","<",">","?0","'"];
+var numericInt = ["0","1","2","3","4","5","6","7","8","9"];
 
 // selecting button in index file
 
@@ -10,72 +14,16 @@ function writePassword() {
 
     // ask user password length 
 
-    let passLength = prompt("How long would you like your password to be?"); 
+    // let passLength = prompt("How long would you like your password to be?"); 
 
     // giving instructions to generatePassword 
 
-    function generatePassword(passLength) { 
-
-        // converting what is entered in prompt to a number between 8 and 128. 
-
-        passLength = parseInt(passLength);
-        if (passLength < 8 || passLength > 128) {
-            alert("Your password must be between 8 and 128 characters");
-
-            // the return stops the function from generating a password even if the condition wasn't met 
-
-            return;
-        };
-
-        // let passChar be empty because we don't know what user will want 
-
-        let passChar = "";
-
-        // adds additional promps to user requesting specific character types they want in password 
-
-        let lowercaseOption = prompt("Would you like lowercase letters in your password? (yes/no)");
-        if (lowercaseOption === "yes") {
-            passChar += "abcdefghijklmnopqrstuvwxyz";
-        };
-
-        let uppercaseOption = prompt("Would you like uppercase letters in your password? (yes/no)");
-        if (uppercaseOption === "yes") {
-            passChar += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        };
-
-        let numericOption = prompt("Would you like numbers in your password?");
-        if (numericOption === "yes") {
-            passChar += "0123456789";
-        };
-
-        let specialOption = prompt("Would you like special characters in your password?");
-        if (specialOption === "yes") {
-            passChar += "!@#$%&()";
-        };
-
-        // the ! checks to see if there is any length of characters inside passChar. 
-        // If there is no length the the following alert message will be displayed 
-
-        if (!passChar.length) {
-            alert("Please press generate button again and say yes to atleast one option.");
-            return;
-        };
-
-        // let passChar = "!@#$%&abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; 
-        let genPass = "";
-
-        // randomly select characters from passChar string that is below the 
-        // enterd number in prompt then adds 1 
-
-        for (let i = 0; i < passLength; i++) {
-           genPass += passChar.charAt(Math.floor(Math.random() * passChar.length));
-        };  
-        return genPass; 
-    }
+    
 
     // putting the result of generatePassword into password variable 
 
-    let password = generatePassword(passLength); 
+    let password = generatePassword(); 
+    console.log(password)
 
     // this is the location in the index where generated password will be printed 
 
@@ -86,6 +34,104 @@ function writePassword() {
     passwordText.value = password;
 };
 
+function generatePassword() { 
+
+    // converting what is entered in prompt to a number between 8 and 128. 
+
+    // passLength = parseInt(passLength);
+    var passLength = parseInt(prompt("How many characters would you like your password?"));
+    if (passLength < 8) {
+        alert("Password length should be atleast 8 characters.");
+        return null;
+    }
+    if (passLength > 128) {
+        alert("Password length must be less than 129 characters.");
+        return null;
+    }
+    // if (passLength < 8 || passLength > 128) {
+    //     alert("Your password must be between 8 and 128 characters");
+
+    //     // the return stops the function from generating a password even if the condition wasn't met 
+
+    //     return;
+    // };
+
+    // let passChar be empty because we don't know what user will want 
+
+    let passChar = [];
+    let  guaranteedChar = [];
+    let result = [];
+
+    // adds additional promps to user requesting specific character types they want in password 
+
+    let lowercaseOption = confirm("Would you like lowercase letters in your password?");
+    let uppercaseOption = confirm("Would you like uppercase letters in your password?");
+    let numericOption = confirm("Would you like numbers in your password?");
+    let specialOption = confirm("Would you like special characters in your password?");
+   
+    if (specialOption) {
+        passChar = passChar.concat(specialChar);
+        guaranteedChar.push(randomChoice(specialChar));
+    };
+
+    if (lowercaseOption) {
+        passChar = passChar.concat(lowerCase);
+        guaranteedChar.push(randomChoice(lowerCase));
+    };
+
+    if (uppercaseOption) {
+        passChar = passChar.concat(upperCase);
+        guaranteedChar.push(randomChoice(upperCase));
+    };
+
+    if (numericOption) {
+        passChar = passChar.concat(numericInt);
+        guaranteedChar.push(randomChoice(numericInt));
+    };
+
+    for (var i = 0; i < passLength; i++) {
+        var passChars = randomChoice(passChar);
+    
+        result.push(passChars);
+      };
+      for (var i = 0; i < guaranteedChar.length; i++) {
+        result[i] = guaranteedChar[i];
+      };
+
+      return result.join('');
+
+    // the ! checks to see if there is any length of characters inside passChar. 
+    // If there is no length the the following alert message will be displayed 
+
+    // if (!passChar.length) {
+    //     alert("Please press generate button again and say yes to atleast one option.");
+    //     return;
+    // };
+
+    // let passChar = "!@#$%&abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; 
+    // let genPass = "";
+
+    // randomly select characters from passChar string that is below the 
+    // enterd number in prompt then adds 1 
+    console.log(specialOption);
+    console.log(numericOption);
+    console.log(uppercaseOption);
+    console.log(lowercaseOption);
+    // for (let i = 0; i < passLength; i++) {
+    //     Math.floor(Math.random() * passChar.length);
+    // };  
+    // return genPass; 
+}
+
+function randomChoice(arr) {
+    var randIndex = Math.floor(Math.random() * arr.length);
+    var randElement = arr[randIndex];
+  
+    return randElement;
+  }
+
 // when button is clicked execute writePassword function printing the generated password 
 
 generateBtn.addEventListener("click", writePassword);
+
+console.log()
